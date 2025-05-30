6ddd227beb4d41f3af83682c0f4eae3d00c4f107
AmitNachum <amitn0@hotmail.co.il>
2025-05-30 23:04:29 +0300

    Change

--- container/main.cpp
+++ container/main.cpp
@@ -1,24 +1,101 @@
+#include <iostream>
 #include "my_container.hpp"
 
+using namespace std;
 using namespace amit_container;
+#define RESET   "\033[0m"
+#define RED     "\033[1;31m"
+#define GREEN   "\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define BLUE    "\033[1;34m"
+#define MAGENTA "\033[1;35m"
+#define CYAN    "\033[1;36m"
+#define WHITE   "\033[1;37m"
+#define BRIGHT_BLACK   "\033[1;30m"
+#define BRIGHT_RED     "\033[1;31m"
+#define BRIGHT_GREEN   "\033[1;32m"
+#define BRIGHT_YELLOW  "\033[1;33m"
+#define BRIGHT_BLUE    "\033[1;34m"
+#define BRIGHT_MAGENTA "\033[1;35m"
+#define BRIGHT_CYAN    "\033[1;36m"
+#define BRIGHT_WHITE   "\033[1;37m"
+#define DARK_BROWN "\033[38;5;94m"
+#define DARK_RED "\033[38;2;139;0;0m"
 
 
-int main(){
 
-    My_Container<int> cont;
-    
-    for(size_t i = 0 ; i < 3 ; i++){
-        
-        cont.add_element(1);
+
+
+
+
+#include <iostream>
+
+
+void print_title(const std::string& title, const std::string& color) {
+    std::cout << color << "=== " << title << " ===" << RESET << '\n';
+}
+
+template<typename Iterator>
+void print_iterator(Iterator begin, Iterator end){
+    std::cout<< "\n   {";
+    for(auto i = begin ; i != end;){
+        std::cout << *i;
+        ++i;
+
+        if(i!= end){
+            std::cout << ", ";
+        }
     }
+    std::cout << "}\n\n";
+}
+
+int main() {
+    My_Container<int> container;
+    container.add_element(7);
+    container.add_element(15);
+    container.add_element(6);
+    container.add_element(1);
+    container.add_element(2);
+
+    cout << "=== Original Container ===\n";
+    cout << "   Size of container: " << container.size() << "\n\n" << endl;
+    print_title("Container content",YELLOW);
+    cout << "\n";
+    cout << "    " << container <<"\n\n";
+
+    print_title("Ascending Iterator",BLUE);
+    print_iterator(container.begin_ascending(), container.end_ascending());
+    print_title("End Ascending",BLUE);
+    cout <<"\n\n";
+
+    print_title("Descending Iterator",GREEN);
+    print_iterator(container.begin_descending(),container.end_descending());
+    print_title("End Descending",GREEN);
+    cout << "\n\n";
+
+    print_title("Side Cross Iterator",MAGENTA);
+    print_iterator(container.begin_side_cross(),container.end_side_cross());
+    print_title("Side Cross End",MAGENTA);
+    cout << "\n\n";
+
+    print_title("Reverse Iterator",RED);
+    print_iterator(container.begin_reverse(),container.end_reverse());
+    print_title("Reverse End",RED);
+    cout <<"\n\n";
 
 
+    print_title("Order Iterator",DARK_BROWN);
+    print_iterator(container.begin_order(),container.end_order());
+    print_title("End Order",DARK_BROWN);
+    cout <<"\n\n";
 
-    std::cout << cont<<std::endl;
+    
+    print_title("Middle Out Iterator",DARK_RED);
+    print_iterator(container.begin_mid_out(),container.end_mid_out());
+    print_title("Middle Out end",DARK_RED);
 
 
-    cont.remove(1);
+    
 
-    std::cout <<cont << std::endl;
     return 0;
 }
--- container/my_container.hpp
+++ container/my_container.hpp
@@ -3,7 +3,9 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-using std::vector,std::cout;
+#include <typeinfo>
+using std::vector;
+using std::cout;
 
 
 namespace amit_container{
@@ -14,6 +16,14 @@ namespace amit_container{
         vector<T> container;
 
         public:
+        class Ascending_Iterator;
+        class Descending_Iterator;
+        class Side_Cross_Order;
+        class Reverse_Iterator;
+        class Order_Iterator;
+        class Middle_Out_Iterator;
+
+
         My_Container() = default;
         My_Container(My_Container& other):container(other.container){};
         ~My_Container() = default;
@@ -33,7 +43,7 @@ namespace amit_container{
             container.erase(std::remove(container.begin(),container.end(),element),container.end());
         }
 
-        size_t size(){
+        size_t size() const{
             return container.size();
         }
 
@@ -45,24 +55,355 @@ namespace amit_container{
             if (std::next(it) != other.container.end()) {
                 os << ", ";
             }
-            
-            }
+        }
 
             os << "}";
 
         return os;
         }
+     
+    Middle_Out_Iterator begin_mid_out(){
+        int middle = (this->size() / 2);
+        Middle_Out_Iterator iter(container);
+        iter.set_index(middle);
+        return iter;
+    }    
+
+    Middle_Out_Iterator end_mid_out(){
+      int end = (this->size() % 2 != 0) ? this->size() : 0;
+      Middle_Out_Iterator iter(container);
+      iter.set_index(end);
+      return iter; 
+    }
 
 
+    Order_Iterator begin_order(){
+        return Order_Iterator(container);
+    }
+    
+    Order_Iterator end_order(){
+        Order_Iterator iter(container);
+        iter.set_index(container.size());
+        return iter;
+    }
+
+    Reverse_Iterator begin_reverse(){
+       return  Reverse_Iterator(container);
+    }
+    Reverse_Iterator end_reverse(){
+        Reverse_Iterator iter(container);
+        iter.set_index(container.size());
+        return iter;
+    }
+
+    Ascending_Iterator begin_ascending(){
+     return Ascending_Iterator(container);
+    }
+
+    Ascending_Iterator end_ascending(){
+       Ascending_Iterator asc_it(container);
+       asc_it.set_index(container.size());
+       return asc_it;
+    }        
+
+    Descending_Iterator begin_descending(){
+        return Descending_Iterator(container); 
+    }
+    
+    Descending_Iterator end_descending(){
+        Descending_Iterator it(container);
+        it.set_index(container.size());
+        return it;
+    }
+    
+
+    Side_Cross_Order begin_side_cross(){
+        return Side_Cross_Order(container);
+    }
+
+    Side_Cross_Order end_side_cross(){
+       Side_Cross_Order iterator(container);
+       iterator.set_index(container.size());
+       return iterator;
+    }
 
 
+class Middle_Out_Iterator{
+    
+    private:
+    vector<T> arrange;
+    size_t index;
+    size_t left;
+    size_t right;
+    size_t middle;
+    bool went_left;
+
+    public:
+
+    Middle_Out_Iterator(const vector<T> data): arrange(data),index(0),went_left(false){
+        middle = arrange.size() / 2;
+        right = middle + 1;
+        left = middle - 1;
     };
+    Middle_Out_Iterator(const Middle_Out_Iterator& other) = default;
+    ~Middle_Out_Iterator() = default;
+    Middle_Out_Iterator& operator=(const Middle_Out_Iterator& other) = default;
 
+
+    T operator*(){
+        return arrange[index];
+    }
+
+ Middle_Out_Iterator& operator++() {
+    if (went_left && right < arrange.size()) {
+        index = right++;
+    } else if (!went_left && left < arrange.size()) {
+        index = left--;
+    } else {
+        index = arrange.size();  // terminate
+    }
+    went_left = !went_left;
+    return *this;
+}
+
+
+
+    bool operator!=(const Middle_Out_Iterator& other){
+        return this->index != other.index;
+    }
+
+    void set_index(size_t changed){
+        if(changed > arrange.size()){
+            throw std::runtime_error("Index Out Of Bounce");
+        }
+        index = changed;
+    }
+
+
+};    
+
+
+class Order_Iterator{
+
+
+    private:
+    vector<T> arrange;
+    size_t index;
+
+    public:
+    Order_Iterator(const vector<T> data):arrange(data),index(0){};
+    Order_Iterator(const Order_Iterator& other) = default;
+    ~Order_Iterator() = default;
+    Order_Iterator& operator=(const Order_Iterator& other) = default;
+
+    void set_index(size_t changed){
+        if(changed > arrange.size()){
+            throw std::runtime_error("Index Out Of Bounce");
+        }
+        index = changed;
+    }
+
+
+    T operator*(){
+        return arrange.at(index);
+    }
+
+    bool operator!=(const Order_Iterator& other){
+        return this->index != other.index;
+    }
+
+    Order_Iterator& operator++(){
+        ++index;
+        return *this;
+    }
+
+};    
+    
+
+    
+
+    
+
+class Ascending_Iterator{
+    private:
+    vector<T> sorted;
+    size_t index;
+
+    public:
+    Ascending_Iterator(const vector<T>& data): sorted(data),index(0){
+        std::sort(sorted.begin(),sorted.end());
+    }
+
+     ~Ascending_Iterator() = default;
+    Ascending_Iterator& operator=(const Ascending_Iterator& other) = default;
+
+    void set_index(size_t changed){
+        if(changed > sorted.size()){
+            throw std::runtime_error("Index Out Of Bounce");
+        }
+        index = changed;
+    }
+
+    T operator*() const{
+        return sorted.at(index);
+    }
+
+    Ascending_Iterator& operator++(){
+        ++index;
+        return *this;
+    }
+
+    bool operator!=(const Ascending_Iterator& other){
+       return this->index != other.index;
+    }
 
 
 
 
 };
+
+
+
+class Descending_Iterator{
+    private:
+    vector<T> sorted;
+    size_t index;
+
+    public:
+
+    Descending_Iterator(const vector<T>& data):sorted(data){
+        std::sort(sorted.begin(), sorted.end(), std::greater<T>());
+        index = 0;
+    }
+    ~Descending_Iterator() = default;
+    Descending_Iterator& operator=(const Descending_Iterator& other) = default;
+
+    T operator*() const{
+        return sorted.at(index);
+    }
+
+    void set_index(size_t changed){
+        if( changed > sorted.size()){
+            throw std::runtime_error("Index Out Of Bounce");
+        }
+        index = changed;
+    }
+
+    Descending_Iterator& operator++(){
+        ++index;
+        return *this;
+    }
+
+    bool operator!=(const Descending_Iterator& other){
+        return this->index != other.index;
+    }
+
+
+
+
+};
+
+class Side_Cross_Order{
+
+    private:
+    size_t index;
+    vector<T> sorted;
+    size_t right;
+    size_t left;
+    bool from_left;
+
+    public:
+
+    Side_Cross_Order(const vector<T>& data)
+        :sorted(data), right(sorted.size() - 1), left(0), from_left(true) {
+            std::sort(sorted.begin(),sorted.end());
+            index = data.size;
+        }
+
+    ~Side_Cross_Order() = default;
+    Side_Cross_Order(const Side_Cross_Order& other) = default;
+    Side_Cross_Order& operator=(const Side_Cross_Order& other) = default;
+
+    T operator*()const{
+        return sorted.at(index);
+    }
+
+    void set_index(size_t changed){
+        if(changed > sorted.size()){
+            throw std::runtime_error("Index Out Of Bounce");
+        }
+        index = changed;
+    }
+
+    Side_Cross_Order& operator++() {
+        if (left > right) {
+            index = sorted.size();  // Mark as end
+            return *this;
+        }
+
+        if (from_left) {
+            index = left++;
+        } else {
+            index = right--;
+        }
+
+        from_left = !from_left;
+        return *this;
+    }
+
+
+
+    bool operator!=(const Side_Cross_Order& other) const {
+        return this->index != other.index;
+    }
+
+
+
+};
+
+
+
+class Reverse_Iterator {
+    private:
+        vector<T> reversed;
+        size_t index;
+
+    public:
+        Reverse_Iterator(const vector<T>& data) : reversed(data), index(0) {
+            std::reverse(reversed.begin(), reversed.end());
+        }
+
+        T operator*() const {
+            return reversed.at(index);
+        }
+
+        Reverse_Iterator& operator++() {
+            ++index;
+            return *this;
+        }
+
+        bool operator!=(const Reverse_Iterator& other) const {
+            return index != other.index;
+        }
+
+    void set_index(size_t changed){
+        if(changed > reversed.size()){
+            throw std::runtime_error("Index Out Of Bounce");
+        }
+        index = changed;
+    }
+    };
+};
+
+
+
+
+};
+
+
+
+
+
 
 
 #endif
